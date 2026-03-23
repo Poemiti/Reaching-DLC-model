e4ead1a19098921ac7ae7008dba90a9ca12e9f60
Poemiti <poemiti.duprat@gmail.com>
2026-03-23 16:32:09 +0100

    initialisation with creation of the directoties

---
+++ src/0.initialisation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from src.reaching_model_utils.config import load_config
+
+cft = load_config()
+
+print("Creating path system... \nThe following folders have been created :")
+
+for _, path in cft.paths : 
+    print("\t", path)
+    path.mkdir(parents=True, exist_ok=True)
